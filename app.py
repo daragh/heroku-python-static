841130e624561fff44f7c2931af960483a50ce2b
Daragh Casey <daragh.casey@gmail.com>
2016-05-23 17:16:57 +0100

    allow serving of multiple static files

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-def app(environ, start_response):
-      with open('public/index.html') as f:
-        data = f.read()
-      start_response("200 OK", [
-          ("Content-Type", "text/html"),
-          ("Content-Length", str(len(data)))
-      ])
-      return iter([data])
+import os
 
+
+def file_paths(path):
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for filename in filenames:
+            yield os.path.join(dirpath, filename)
+
+
+def app(environ, start_response):
+    root = 'public'
+    route = 'index.html' if environ['RAW_URI'] == '/' else environ['RAW_URI'][1:]
+    routes = [path[len(root) + 1:] for path in file_paths(root)]
+    (response, data) = (
+        ('200 OK', open(os.path.join(root, route)).read()) if
+        route in routes
+        else ('404 Not Found', 'Not found.'))
+    start_response(response, [('Content-Length', str(len(data)))])
+    return iter([data])
